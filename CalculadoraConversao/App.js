537cb225ace0d20889cf9c719342e42b44e94dcd
douglasdans <douglas.dans@gmail.com>
2024-09-06 20:54:28 -0300

    feat: adiciona mini projeto 2

---
+++ CalculadoraConversao/App.js
@@ -0,0 +1,71 @@
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  Button,
+  Alert,
+  ToastAndroid,
+} from "react-native";
+import styles from "./app.styles";
+
+export default function App() {
+  const [celsius, setCelsius] = useState("");
+  const [kmh, setKmh] = useState("");
+
+  // Função de conversão de Celsius para Fahrenheit
+  function convertTemperature() {
+    const celsiusValue = parseFloat(celsius);
+    if (isNaN(celsiusValue)) {
+      Alert.alert("Erro", "Insira um valor válido para Celsius");
+      return;
+    }
+    const fahrenheitValue = (celsiusValue * 9) / 5 + 32;
+    ToastAndroid.show(
+      `Fahrenheit: ${fahrenheitValue.toFixed(4)}`,
+      ToastAndroid.SHORT
+    );
+  }
+
+  // Função de conversão de km/h para mph
+  const convertSpeed = () => {
+    const kmhValue = parseFloat(kmh);
+    if (isNaN(kmhValue)) {
+      Alert.alert("Erro", "Insira um valor válido para km/h");
+      return;
+    }
+    const mphValue = kmhValue * 0.621371;
+    ToastAndroid.show(`mph: ${mphValue.toFixed(4)}`, ToastAndroid.SHORT);
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Calculadora de Conversão</Text>
+
+      <View>
+        <Text style={styles.label}>Temperatura (ºC para ºF)</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Insira a temperatura em ºC"
+          value={celsius}
+          onChangeText={setCelsius}
+        />
+        <Button title="Converter para ºF" onPress={convertTemperature} />
+      </View>
+
+      <View>
+        <Text style={styles.label}>Velocidade (km/h para mph)</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType="numeric"
+          placeholder="Insira a velocidade em km/h"
+          value={kmh}
+          onChangeText={setKmh}
+        />
+        <Button title="Converter para mph" onPress={convertSpeed} />
+      </View>
+    </View>
+  );
+}
---
+++ CalculadoraConversao/app.styles.js
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    padding: 20,
+    gap: 40,
+    backgroundColor: "#f0f8ff",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+  input: {
+    height: 40,
+    borderColor: "gray",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    marginBottom: 20,
+    borderRadius: 5,
+  },
+  result: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+});
+
+export default styles;
